10195215f8c1258ebc1e9b16c97b33917b4a8833
Red Hat <sonjuhy@gmail.com>
2023-05-25 00:07:18 +0900

    ✨ ADD Kafka Consumer Thread
    - ADD Kafka Consumer Thread

--- MyHome/Kafka/Kafka_Consumer.py
+++ MyHome/Kafka/Kafka_Consumer.py
@@ -1,26 +1,27 @@
-from json import loads
-from time import sleep
+import threading
+from threading import Thread
+
 from kafka import KafkaConsumer
 
+class Kafka_Consumer_default:
 
-consumer = KafkaConsumer(
-    'exam-topic',
-    bootstrap_servers=['192.168.0.254:9092'],
-    auto_offset_reset='earliest',
-    enable_auto_commit=True,
-    group_id='django-kafka',
-    # value_deserializer=lambda x: loads(x.decode('utf-8')),
-    consumer_timeout_ms=1000
-)
+    def run(self, topic):
+        task = threading.Thread(target=self.listen, args=[topic])
+        task.setDaemon(True)
+        task.start()
 
-def listen():
-    print('Starting Consumer')
-
-    while True:
-        message = consumer.poll()
-        if len(message) != 0:
-            print("consumer")
-            for topic, recodes in message.items():
-                for recode in recodes:
-                    print("recode :{}".format(recode.value))
+    def listen(self, topic):
+        print('Starting listening')
+        consumer = KafkaConsumer(
+            topic,
+            bootstrap_servers=['192.168.0.254:9092'],
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            group_id='django-kafka',
+            # value_deserializer=lambda x: loads(x.decode('utf-8')),
+            consumer_timeout_ms=1000
+        )
+        while True:
             consumer.commit()
+            for message in consumer:
+                print("Topic: {}, Partition: {}, Offset: {}, Key: {}, Value: {}".format( message.topic, message.partition, message.offset, message.key, message.value.decode('utf-8')))
