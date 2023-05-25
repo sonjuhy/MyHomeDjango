3ea547a38d8a0edb7239f5338f8e7bd48b289b79
Red Hat <sonjuhy@gmail.com>
2023-05-25 13:20:04 +0900

    ✨ ADD light control code in kafka consumer
    - Add MQTT pub code in consumer

--- MyHome/Kafka/Kafka_Consumer.py
+++ MyHome/Kafka/Kafka_Consumer.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import json
 import threading
 from threading import Thread
 
@@ -21,7 +24,22 @@ class Kafka_Consumer_default:
             # value_deserializer=lambda x: loads(x.decode('utf-8')),
             consumer_timeout_ms=1000
         )
+
+        sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+        from MQTT import publisher
+        from MQTT import jsonParser
+        
         while True:
             consumer.commit()
             for message in consumer:
                 print("Topic: {}, Partition: {}, Offset: {}, Key: {}, Value: {}".format( message.topic, message.partition, message.offset, message.key, message.value.decode('utf-8')))
+                value = message.value.decode('utf-8')
+                if json.loads(value):
+                    parsing_data = jsonParser.JSON_Parser_android(value)
+                    publisher.pub('MyHome/Light/Pub/'+parsing_data['room'],value)
+                    print('parsing_data on Kafka: %s' % value)
+                else:
+                    print('value is not JSON')
+
+
+                
